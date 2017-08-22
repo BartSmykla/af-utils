e9c6cac58a5df13e766d7adc33405f963f5249df
Bartłomiej Smykla <bartek@smykla.com>
2017-08-22 16:06:25 +0200

    added jsdoc comments to camelize function

--- src/camelize/camelize.js
+++ src/camelize/camelize.js
@@ -1,3 +1,9 @@
+/**
+ * Simple function for change string from snake_case to camelCase
+ * 
+ * @param {string} str - string which will be camelized
+ * @returns {string} camelized string
+ */
 const camelize = str =>
   str.replace(/^([A-Z])|[\s-_](\w)/g, (match, p1, p2) => {
     if (p2)
