69aabe5c17c74516d1f132a467df177d70e49aa2
Bartłomiej Smykla <bartek@smykla.com>
2017-08-22 14:53:43 +0200

    small refactoring in camelize function

--- src/camelize/camelize.js
+++ src/camelize/camelize.js
@@ -1,10 +1,9 @@
-const camelize = (str) => (
+const camelize = str =>
   str.replace(/^([A-Z])|[\s-_](\w)/g, (match, p1, p2) => {
     if (p2)
       return p2.toUpperCase();
 
     return p1.toLowerCase();
-  })
-);
+  });
 
 module.exports = camelize;
