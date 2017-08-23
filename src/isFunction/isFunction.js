3840b946cf199dbbfe04653151972b59694c6da9
Bartłomiej Smykla <bartek@smykla.com>
2017-08-23 09:23:56 +0200

    isFunction documentation

--- src/isFunction/isFunction.js
+++ src/isFunction/isFunction.js
@@ -1,3 +1,22 @@
-const isFunction = fn => fn && {}.toString.call(fn) === '[object Function]';
+/**
+ * Simple function which is checking if given argument is a function
+ * 
+ * @param {any} item anything which should be checked if is function
+ * @example
+ * const noop = () => {};
+ * isFunction(noop); // returns true
+ * @example
+ * function noop() {};
+ * isFunction(noop); // returns true
+ * @example
+ * const noop = function() {};
+ * isFunction(noop); // returns true
+ * @example
+ * isFunction({}); // returns false
+ * @example
+ * isFunction('string'); // returns false
+ * @returns {boolean} true if argument is function, false if not
+ */
+const isFunction = item => item && {}.toString.call(item) === '[object Function]';
 
 module.exports = isFunction;
