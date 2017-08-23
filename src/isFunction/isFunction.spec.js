cdba21005b03d121fad089979707846924265bb5
Bartłomiej Smykla <bartek@smykla.com>
2017-08-23 10:44:06 +0200

    improved using leche isFunction unit tests

--- src/isFunction/isFunction.spec.js
+++ src/isFunction/isFunction.spec.js
@@ -1,24 +1,37 @@
 const { expect } = require('chai');
+const { withData } = require('leche');
 
 const { isFunction } = require('../../');
 
 describe('isFunction', () => {
-  it('should return true when passed argumentt is a function', () => {
-    const fs1 = () => {};
-    const fs2 = function() {};
-    function fs3() {};
+  const args = {
+    true: [
+      () => {},
+      function() {},
+      function fs() {},
+    ],
+    false: [
+      {},
+      'string',
+      undefined,
+      null,
+      new Date(),
+      1,
+      [],
+      [1,2,3,4],
+      Symbol(''),
+    ],
+  }
 
-    expect(isFunction(fs1)).to.be.equal(true);
-    expect(isFunction(fs2)).to.be.equal(true);
-    expect(isFunction(fs3)).to.be.equal(true);
+  withData(args.true, (arg) => {
+    it('should return true when passed argumentt is a function', () => {
+      expect(isFunction(arg)).to.be.equal(true);
+    });
   });
 
-  it('should return false when passed argumentt is any other (than function) type', () => {
-    expect(isFunction({})).to.be.equal(false);
-    expect(isFunction('string')).to.be.equal(false);
-    expect(isFunction(undefined)).to.be.equal(false);
-    expect(isFunction(null)).to.be.equal(false);
-    expect(isFunction(new Date())).to.be.equal(false);
-    expect(isFunction(1)).to.be.equal(false);
+  withData(args.false, (arg) => {
+    it('should return false when passed argumentt is any other (than function) type', () => {
+      expect(isFunction(arg)).to.be.equal(false);
+    });
   });
 });
